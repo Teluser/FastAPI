7632e7ca276933146243a8def64a287defff8057
Linhntt8 <linhntt8@rikkeisoft.com>
2024-09-19 13:40:38 +0700

    Add authentication for APIs with prefix /posts

--- Learn basic fastAPI/app/routers/user.py
+++ Learn basic fastAPI/app/routers/user.py
@@ -13,7 +13,7 @@ def get_users(db:Session = Depends(get_db)):
     return crud.get_users(db)
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.User)
-def creare_user(user: schemas.UserCreate, db:Session = Depends(get_db)):
+def create_user(user: schemas.UserCreate, db:Session = Depends(get_db)):
     if crud.get_user_by_email(db, email=user.email):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
     return crud.create_user(db, user)
